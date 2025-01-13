e544f9d1b204aa18dbc20a775cf195bd6ebe4422
Mattia-Carbonera <mattia.carbonera@gmail.com>
2025-01-13 18:15:05 +0100

    fix reviews section

--- components/MovieDetalist/MovieDetalist.jsx
+++ components/MovieDetalist/MovieDetalist.jsx
@@ -4,23 +4,30 @@ import { useParams } from "react-router-dom";
 export default function MovieDetalist() {
   const [movie, setMovie] = useState([]);
 
-  const bookId = useParams();
-  const urlFrontend = "http://localhost:3000/db/" + bookId.id;
+  const movieId = useParams();
+  const urlFrontend = "http://localhost:3000/db/" + movieId.id;
 
   useEffect(() => {
     fetch(urlFrontend)
       .then((res) => res.json())
       .then((data) => {
         setMovie(data);
+        // console.log(data);
       });
   }, []);
+
+  console.log(movie);
 
   return (
     <main>
       <h1>Movie Detail</h1>
       <div className="movie-detalis-container">
         <div className="movie-detalis">
-          {/* <h2>{movie[0].title}</h2> */}
+          <h2>{movie?.length > 0 && movie[0].title}</h2>
+          {/* {movie.map((mov) => (
+            <h2 key={mov.id}>{mov.title}</h2>
+          ))} */}
+          {/* <h2>{movie.title}</h2> */}
           <hr className="movie-detalis-hr" />
           <h3>Review:</h3>
           <ol className="reviews-list">
